b83a141540fc69aa8dba97a86cb2b27a06afd3b4
n3wborn <n3wborn@protonmail.com>
2020-06-10 18:42:37 +0200

    Ajoute le php pour la connection a bdd

---
+++ db-connect.php
@@ -0,0 +1,11 @@
+<?php
+$dsn = 'mysql:dbname=stephanep460_;host=localhost';
+$user = 'stephanep460';
+$password = 'lWKt+esA1AvVhA==';
+
+try {
+  $base = new PDO($dsn, $user, $password);
+  $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo 'Connexion échouée : ' . $e->getMessage();
+}
