a273014e08b50e23e4a5f69b022d8cb437b0d0d8
n3wborn <n3wborn@protonmail.com>
2020-06-10 09:41:07 +0200

    Insertion des donnees dans la table apprenants

--- requetes.sql
+++ requetes.sql
@@ -43,3 +43,23 @@ CREATE TABLE apprenants (
   departement INT(3) NOT NULL,
   PRIMARY KEY (ID)
 );
+
+-- 9 - Insertion des données dans la tables apprenants
+INSERT INTO `apprenants` (prenom, nom, departement)
+VALUES
+ ('Adrien','PATUROT','25'),
+ ('Adrien','SCHMIDT','25'),
+ ('Jessica','DA SILVA ','39'),
+ ('Alexandre','VIGNEAUD','25'),
+ ('Arnaud','DELCROIX','25'),
+ ('Clovis','DAUBOURG','25'),
+ ('Fouad','LYOUSFI','25'),
+ ('Guillaume','BLONDEL','25'),
+ ('Guillaume','PERBET','25'),
+ ('Jérémy','MATHIEU','25'),
+ ('Lisa','DEVOGHELAERE','25'),
+ ('Léa','ZERVINI','25'),
+ ('Sonia','HOAREAU','70'),
+ ('Stéphane','PLUCHART','70'),
+ ('Vanessa','KNORR','25');
+
