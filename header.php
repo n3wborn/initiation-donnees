6648018f0f947a7f446c654ea15fb79a266d483b
n3wborn <n3wborn@protonmail.com>
2020-06-11 08:45:00 +0200

    Ajout du fichier de base header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico">
+    <title>Initiation Aux Données</title>
+</head>
+
+<body>
+	<header>
+		<menu>
+			<nav>
+				<ul>
+					<li>Exercice 1</li>
+					<li>Exercice 2</li>
+					<li>Exercice 3</li>
+					<li>Exercice 4</li>
+					<li>Exercice 5</li>
+					<li>Exercice 6</li>
+					<li>Exercice 7</li>
+					<li>Exercice 8</li>
+					<li>Exercice 9</li>
+				</ul>
+			</nav>
+
+	  <h1>Initiation aux Données</h1>
+  
